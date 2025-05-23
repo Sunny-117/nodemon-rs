dca430e772f8237920b698f291a166fc6f21c6a4
Sunny-117 <zhiqiangfu6@gmail.com>
2025-05-23 15:31:10 +0800

    feat: nodemon exec order

---
+++ example/index.js
@@ -0,0 +1,7 @@
+console.log('This is the default index.js file');
+console.log('Current time:', new Date().toISOString());
+
+// Add some async operation to keep the process running
+setInterval(() => {
+    console.log('Heartbeat:', new Date().toISOString());
+}, 2000); 
